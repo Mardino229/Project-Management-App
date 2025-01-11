ebb9823cdf480b636cce22d20e1911dccbcc1580
Maxime Mardino DASSI <136202839+Mardino229@users.noreply.github.com>
2025-01-11 14:12:49 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-/root/.config/herd-lite/bin/composer install --no-interaction --optimize-autoloader
+/root/.config/herd-lite/bin/composer install --no-interaction --optimize-autoloader --ignore-platform-req=ext-dom --ignore-platform-req=ext-xml
 echo "Packages installed"
 php artisan key:generate --force
 php artisan db:seed --force
